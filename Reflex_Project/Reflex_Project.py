a6c03d69f4b9409624df8c044c72e588685c04d3
배수한 <baesuhan@baesuhan-ui-MacBookAir.local>
2025-04-08 13:19:37 +0900

    4/8 commit

--- Reflex_Project/Reflex_Project.py
+++ Reflex_Project/Reflex_Project.py
@@ -1,7 +1,7 @@
 import reflex as rx
-from week01 import text_practice
+from week01 import text_practice,button_practice
 
 app = rx.App()
-app.add_page(text_practice.index)
-
+app.add_page(text_practice.index,route = "/text")
+app.add_page(button_practice.index,route = "/button")
 # control + c : Reflex stop
--- week01/button_practice.py
+++ week01/button_practice.py
@@ -1,2 +1,37 @@
+import reflex as rx
 
+class State(rx.State) :
+    count = 0
     
+    def increase(self) :
+        self.count += 1
+        
+    def decrease(self) :
+        self.count -= 1
+        
+def index():
+    return rx.center(
+        rx.button(
+            "Decrease",
+            color = "white",
+            bg = "red",
+            size = "3",
+            variant = "soft",
+            radius = "large",
+            on_click = State.decrease
+        ),
+        rx.text(f"Clicked : {State.count}",
+                font_size = "50px",
+                font_weight = "bold",
+                ),
+        rx.button(
+            "Increase",
+            color = "white",
+            bg = "blue",
+            size = "3",
+            variant = "soft",
+            radius = "large",
+            on_click = State.increase),
+        height = "100vh",
+        width = "100vm"
+    )
